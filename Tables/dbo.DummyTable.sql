f73daf07d956f55e5139f95e2b97c05d43065ca5
Prakash Arulanantham <eee_prakash@rediffmail.com>
2015-10-05 18:38:22 +0000

    Added New Column to Dummy Table

--- Tables/dbo.DummyTable.sql
+++ Tables/dbo.DummyTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[DummyTable]
 (
 [ID] [int] NOT NULL,
-[Name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[New Column] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
